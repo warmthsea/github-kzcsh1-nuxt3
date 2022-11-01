45bf28c7d21ac89b23d39d83249ce440964125a2
huayang wang <2586244885@qq.com>
2022-11-01 12:43:27 +0800

    test: api request

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    vite: {
+        server: {
+            open: true,
+            strictPort: true,
+            proxy: {
+                '/equipment': 'http://192.168.10.137:9999',
+                '/cms': {
+                    target: 'http://192.168.10.137:9999',
+                    ws: true,
+                    changeOrigin: true
+                }
+            }
+        },
+    },
 })
